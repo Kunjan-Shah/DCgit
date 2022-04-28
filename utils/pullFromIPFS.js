c3243b947493876d5c0e515684fdbf02f7d3cd24
pratham <pratham1002@gmail.com>
2022-04-28 21:09:36 +0530

    works

--- utils/pullFromIPFS.js
+++ utils/pullFromIPFS.js
@@ -1,5 +1,6 @@
 const util = require('util');
 const AdmZip = require('adm-zip');
+const ipfsAPI = require('ipfs-api');
 
 /**
  * Retrieves a file from IPFS and decrypts it with decipher
@@ -11,13 +12,14 @@ async function pullFromIPFS(ipfsAddress, decipher) {
     //Connceting to the ipfs network via infura gateway
     const ipfs = ipfsAPI('ipfs.infura.io', '5001', { protocol: 'https' })
 
-    ipfs.files.get(validCID, function (err, files) {
-        files.forEach((file) => {
-          console.log(file.path)
-          console.log(file.content.toString('utf8'))
-        })
-    })
-    return "TODO"
+    const fileGet = util.promisify(ipfs.files.get)
+
+    const file = await fileGet(ipfsAddress)
+
+
+    const zippedGit = decipher.update(file[0].content, 'binary', 'utf-8') + decipher.final('utf8');
+
+    return zippedGit
 }
 
 module.exports  = pullFromIPFS
