17f146aabfc192be8de27cdb7d52c6dc36566aca
KUNJAN <kunjan.nshah1@gmail.com>
2022-05-01 16:42:00 +0530

    init smart contract working

--- commands/pull.js
+++ commands/pull.js
@@ -15,7 +15,7 @@ async function pull({ branch }) {
         const iv = await privateKeyDecryption(dcgit.userPrivateKey, dcgit.encryptedIV);
 
         const decipher = crypto.createDecipheriv('aes256', Buffer.from(encryptionKey, 'hex'), Buffer.from(iv, 'hex'));
-
+        // TODO: call getRepoInfo --> storage_adress, integrity update in .dcgit.json
         const zippedGit = await pullFromIPFS(dcgit.ipfsAddress, decipher);
 
         await syncRepo(branch, zippedGit);
