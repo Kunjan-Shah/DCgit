6e09eca2f234d7298c97389fd125c2aae4e282d3
pratham <pratham1002@gmail.com>
2022-05-02 10:39:52 +0530

    working

--- commands/permit.js
+++ commands/permit.js
@@ -1,15 +1,63 @@
-const chalk = require('chalk')
+const fs = require('fs')
+const chalk = require('chalk');
+const EthCrypto = require('eth-crypto');
+const publicKeyEncryption = require('../utils/encryptWithPublicKey');
+const { contractInstance, web3, contractAddress } = require('../contract');
 
-function permit(role, identity) {
-    /******* TODO *********/
-    // get repo "uuid" from .dcgit
-    // generate "key" which is encrypted by "identity" public key
-    // generate "address" using "identity" public key
-    // convert role sting to enum index for read/write access respectively
-    // call grant_access(uuid, address, role, key)
-    // Error handling
-    
-    console.log(chalk.greenBright("Permission added successfully successfully"));
+async function permit({ role, identity }) {
+    try {
+        // verify that the role is either "read" or "write"
+        if (role !== 'read' && role !== 'write') {
+            console.log(chalk.red('Error: Role must be either "read" or "write".'))
+            return
+        }
+
+        // read .dcgit.json
+        const fileExist = fs.existsSync('./.dcgit.json');
+        if (!fileExist) {
+            console.log(chalk.redBright('No dcgit.json file found. Please run "dcgit setup" first.'));
+            return;
+        }
+
+        const dcgit = JSON.parse(fs.readFileSync('./.dcgit.json', 'utf8'))
+
+        if (dcgit.uuid === undefined) {
+            console.log(chalk.redBright('DCGit repo not initialized. Please run "dcgit init" first.'));
+            return;
+        }
+
+        const key = dcgit.key;
+        const iv = dcgit.iv;
+
+        // encrypt the key with the identity public key
+        const encryptedKey = await publicKeyEncryption(identity, key);
+        const encryptedIV = await publicKeyEncryption(identity, iv);
+
+        console.log({ encryptedKey, encryptedIV });
+
+        console.log("Calling Smart Contract for Granting Access");
+
+        const encoded = contractInstance.methods.grantAccess(
+            dcgit.uuid,
+            EthCrypto.publicKey.toAddress(identity),
+            role === 'read' ? 1 : 2,
+            encryptedKey,
+            encryptedIV
+        ).encodeABI()
+
+        const tx = {
+            to: contractAddress,
+            data: encoded,
+            gas: 3000000
+        }
+
+        const signed = await web3.eth.accounts.signTransaction(tx, dcgit.userPrivateKey)
+        await web3.eth.sendSignedTransaction(signed.rawTransaction).on('receipt', console.log)
+
+        console.log(chalk.greenBright("Permission added successfully successfully"));
+    } catch (error) {
+        console.log(chalk.red(error))
+    }
 }
 
 module.exports = permit;
